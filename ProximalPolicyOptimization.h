9568b5fc4f9457d01ee7847bbff14d24e3394bf8
martin <m.huber_1994@hotmail.de>
2019-03-07 01:18:56 +0100

    removed some useless stuff

--- ProximalPolicyOptimization.h
+++ ProximalPolicyOptimization.h
@@ -4,15 +4,6 @@
 
 #include "Models.h"
 
-// Generalized advantage estimate.
-using GAE = std::vector<std::tuple<torch::Tensor /*state*/,
-                                   torch::Tensor /*action*/,
-                                   torch::Tensor /*policy*/,
-                                   torch::Tensor /*return*/,
-                                   torch::Tensor /*advantage*/>>;
-
-
-
 // Vector of tensors.
 using VT = std::vector<torch::Tensor>;
 
@@ -28,8 +19,6 @@ class PPO
 {
 public:
     static auto returns(VT& rewards, VT& dones, VT& vals, double gamma, double lambda) -> VT; // Generalized advantage estimate, https://arxiv.org/abs/1506.02438
-    static auto update(ActorCritic& ac, GAE& gaes, OPT& opt, uint epochs, uint mini_batch_size, double clip_param=.2) -> void; // Update the policy after T time steps for K epochs
-
     static auto update(ActorCritic& ac,
                        torch::Tensor& states,
                        torch::Tensor& actions,
@@ -56,47 +45,6 @@ auto PPO::returns(VT& rewards, VT& dones, VT& vals, double gamma, double lambda)
     }
 
     return returns;
-}
-
-auto PPO::update(ActorCritic& ac, GAE& gaes, OPT& opt, uint epochs, uint mini_batch_size, double clip_param) -> void
-{
-    for (uint i=0;i<epochs;i++)
-    {
-        // Generate random indices.
-        std::vector<uint> idx;
-        idx.reserve(mini_batch_size);
-
-        for (uint i=0;i<mini_batch_size;i++) {
-
-            idx.push_back(std::uniform_int_distribution<uint>(0, gaes.size()-1)(re));
-        }
-
-        for (auto& i: idx)
-        {
-            auto state = std::get<0>(gaes[i]);
-            auto av = ac.forward(state); // action value pairs
-            auto action = std::get<0>(av);
-            auto entropy = ac.entropy();
-            auto new_log_prob = ac.log_prob(action);
-
-            auto old_log_prob = std::get<2>(gaes[i]);
-            auto ratio = (new_log_prob - old_log_prob).exp();
-            auto advantage = std::get<4>(gaes[i]);
-            auto surr1 = ratio*advantage;
-            auto surr2 = torch::clamp(ratio, 1. - clip_param, 1. + clip_param)*advantage;
-
-            auto ret = std::get<4>(gaes[i]);
-            auto val = std::get<1>(av);
-            auto actor_loss = -torch::min(surr1, surr2);
-            auto critic_loss = (ret-val).pow(2);
-
-            auto loss = 0.5*critic_loss+actor_loss-0.001*entropy;
-
-            opt.zero_grad();
-            loss.backward();
-            opt.step();
-        }
-    }
 }
 
 auto PPO::update(ActorCritic& ac,
