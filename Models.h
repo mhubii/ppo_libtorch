426476377720ec7b57485c281214182fb89b9e0e
martin <m.huber_1994@hotmail.de>
2019-04-03 23:47:35 +0200

    added tutorial example

--- Models.h
+++ Models.h
@@ -7,37 +7,51 @@
 struct ActorCriticImpl : public torch::nn::Module 
 {
     // Actor.
-    torch::nn::Linear a_lin1_, a_lin2_, a_lin3_;
+
+    // *** Fill in code ***
+    //
+    //     - add layers to the actor, for example torch::nn::Linear
+
     torch::Tensor mu_;
     torch::Tensor log_std_;
 
     // Critic.
-    torch::nn::Linear c_lin1_, c_lin2_, c_lin3_, c_val_;
+
+    // *** Fill in code ***
+    //
+    //     - add layers to the critic, for example torch::nn::Linear
+
+    torch::nn::Linear c_val_; // value layer
 
     ActorCriticImpl(int64_t n_in, int64_t n_out, double std)
         : // Actor.
-          a_lin1_(torch::nn::Linear(n_in, 16)),
-          a_lin2_(torch::nn::Linear(16, 32)),
-          a_lin3_(torch::nn::Linear(32, n_out)),
+
+          // *** Fill in code ***
+          //
+          //     - initialize the layers, for example actor_layer1_(torch::nn::Linear(n_in, 16))
+
           mu_(torch::full(n_out, 0.)),
           log_std_(torch::full(n_out, std)),
           
           // Critic
-          c_lin1_(torch::nn::Linear(n_in, 16)),
-          c_lin2_(torch::nn::Linear(16, 32)),
-          c_lin3_(torch::nn::Linear(32, n_out)),
+
+          // *** Fill in code ***
+          //
+          //     - initialize the layers
+
           c_val_(torch::nn::Linear(n_out, 1)) 
     {
         // Register the modules.
-        register_module("a_lin1", a_lin1_);
-        register_module("a_lin2", a_lin2_);
-        register_module("a_lin3", a_lin3_);
+
+        // *** Fill in code ***
+        //
+        //     - register the actor layers, for example register_module("actor_layer1", actor_layer1_)
+
         register_parameter("log_std", log_std_);
 
-        register_module("c_lin1", c_lin1_);
-        register_module("c_lin2", c_lin2_);
-        register_module("c_lin3", c_lin3_);
-        register_module("c_val", c_val_);
+        // *** Fill in code ***
+        //
+        //     - register the critic layers
     }
 
     // Forward pass.
@@ -45,14 +59,16 @@ struct ActorCriticImpl : public torch::nn::Module
     {
 
         // Actor.
-        mu_ = torch::relu(a_lin1_->forward(x));
-        mu_ = torch::relu(a_lin2_->forward(mu_));
-        mu_ = torch::tanh(a_lin3_->forward(mu_));
+        // *** Fill in code ***
+        //
+        //     - feed forward the actor, for example x = actor_layer1_->forward(x)
+        //     - add activation functions, for example x = torch::relu(actor_layer1_->forward(x))
 
         // Critic.
-        torch::Tensor val = torch::relu(c_lin1_->forward(x));
-        val = torch::relu(c_lin2_->forward(val));
-        val = torch::tanh(c_lin3_->forward(val));
+        torch::Tensor val = 
+        // *** Fill in code ***
+        //
+        //     - feed forward the critic, add activation functions
         val = c_val_->forward(val);
 
         if (this->is_training()) 
